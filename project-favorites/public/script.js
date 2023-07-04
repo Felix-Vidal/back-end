379132c751fa9066214941ff96ee9f02825a8adf
joao.vidal <112819239+Felix-Vidal@users.noreply.github.com>
2023-07-03 19:07:00 -0500

    funcionalidade salvar

--- project-favorites/public/script.js
+++ project-favorites/public/script.js
@@ -1,26 +1,72 @@
-const links = document.querySelector("ul#links");
+const ul = document.querySelector('ul')
+const input = document.querySelector('input')
+const form = document.querySelector('form')
 
-fetch("http://jsonplaceholder.typicode.com/users") //mudar para onde ta o arquivo JSON
-  .then(resposta => {
-    if (!resposta.ok) {
-      throw new Error("Erro ao obter os links.");
-    }
-    return resposta.json();
-  })
-  .then(data => {
-    data.forEach(link => {
-      const li = document.createElement('li');
-      const a = document.createElement('a');
-      a.href = 'https://chat.openai.com/'; // add link do site `{link.link}
-      // usar {link.name} e {link.link}
-      li.textContent = `${link.name} - ${link.email}`;
-      a.appendChild(li);
-      links.appendChild(a);
-    });
-  })
-  .catch(error => {
-    console.log(error);
-    const li = document.createElement("li");
-    li.textContent = "Erro ao obter os links.";
-    links.appendChild(li);
-});
+/*
+
+// Não se preocupem com esse pedaço de código comentado! Vamos descomentá-lo quando tivermos acabado de construir a API.
+
+// Função que carrega o conteúdo da API.
+async function load() {
+    // fetch está como await para evitar que entre num esquema de promisse e só devolva o conteúdo após a iteração qua acontece em seguida.
+    const res = await fetch('http://localhost:3000/')
+        .then(data => data.json())
+    // Iterando no vetor com o conteúdo (JSON) que está vindo da API e adicionando-os no frontend.
+    res.urls.map(({name, url}) => addElement({name, url}))
+}
+
+load()
+*/
+
+function addElement({ name, url }) {
+  const listItem = document.createElement('li');
+  const link = document.createElement('a');
+  const linkText = document.createTextNode(name);
+
+  link.classList.add('format')
+
+  link.appendChild(linkText);
+  link.href = url;
+  link.target = '_blank';
+
+  const removeButton = document.createElement('button');
+  removeButton.classList.add("remover")
+  removeButton.innerHTML = 'Remover';
+
+  removeButton.addEventListener('click', () => {
+      removeElement(listItem);
+  });
+
+  listItem.appendChild(link);
+  listItem.appendChild(removeButton);
+
+  document.getElementById('links').appendChild(listItem);
+}
+
+function removeElement(element) {
+  element.parentNode.removeChild(element);
+}
+
+
+form.addEventListener('submit', (event) => {
+    
+    event.preventDefault();
+
+    let { value } = input
+
+    if (!value)
+        return alert('Preencha o campo!')
+
+    const [name, url] = value.split(',')
+
+    if (!url)
+        return alert('O texto não está formatado da maneira correta.')
+
+    if (!/^http/.test(url))
+        return alert('Digite a url da maneira correta.')
+
+    addElement({ name, url })
+
+    input.value = ''
+
+})
