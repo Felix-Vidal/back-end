60ba61243b671270b3b588b4875392a9fbcc2b7b
joao.vidal <112819239+Felix-Vidal@users.noreply.github.com>
2023-07-04 20:13:05 -0500

    Roteiro2- Construção de uma api em dupla

--- project-favorites/api/index.js
+++ project-favorites/api/index.js
@@ -1,16 +1,32 @@
 const http = require('http'); //criar o servidor
 const data = require('./urls.json'); //pegar os elementos do json
 const URL = require('url'); //requisições
+const fs = require('fs');
+const path = require('path')
 
+function writeFile(cb){
+    fs.writeFile(
+        path.join(__dirname, 'urls.json'),
+        JSON.stringify(data, null, 2),
+        err => {
+            if (err) throw err
+            cb('Operação realizada com sucesso!')
+        }
+    )
+}
 
 http.createServer((req,res) => {
     const {name, url, del} = URL.parse(req.url, true).query //declarar os elementos da requisição
+    if(!name || !url) return res.end(JSON.stringify(data)); //se não tiver nenhum elemento ele faz a requisição dos dados 
 
-    if(!name || !url) return res.end("show");
-
-    if(del) return res.end('delete');
-
-    return res.end('create');
+    
+    if(del){
+        data.urls = data.urls.filter(item => item.url != url)
+        return writeFile(message => res.end(message))
+    } 
+        
+    data.urls.push({name, url})
+    return writeFile(message => res.end(message))
     
 }).listen(3000, () => console.log('API rodando...'));
 
